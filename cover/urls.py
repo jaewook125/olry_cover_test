55c2a2423e2f9160090d75ccda803c110c3254ca
sjw0484 <sjw0484@naver.com>
2018-03-22 15:43:02 +0900

    pillow 이미지 생성

--- cover/urls.py
+++ cover/urls.py
@@ -5,4 +5,5 @@ app_name = 'cover'
 
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('image.jpg', views.image_generater, name='image_generater'),
 ]
--- cover/views.py
+++ cover/views.py
@@ -1,4 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from PIL import Image
+from .forms import CoverForm
 
 def index(request):
-	return render(request, 'cover/index.html')
+	if request.method == "POST":
+		form = CoverForm(request.POST)
+		if form.is_valid():
+			form.cleaned_data #이미지를 생성할 데이터는 여기에 있다
+			return redirect("cover:index")
+	else:
+		form = CoverForm()
+	return render(request, 'cover/index.html', {
+			'form':form, 
+		})
+
+
+def image_generater(request):
+	im = Image.new("RGB", (256,256))
+	# im : 위 데이터를 받아서, 이미지는 여기에서 그림
+	
+	response = HttpResponse(content_type='image/jpeg') #file-like
+	im.save(response, format='JPEG')
+	return response
