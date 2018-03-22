a61512d7f2da07b833f5abb9ad78404686cf52f0
sjw0484 <sjw0484@naver.com>
2018-03-22 15:10:11 +0900

    cover index 생성

--- orly_cover/urls.py
+++ orly_cover/urls.py
@@ -1,8 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cover/', include('cover.urls', namespace='cover')),
+    path('', lambda r: redirect('cover:index')),
+    #루트주소로 이동시키는 코드 cover/index로
 ]
